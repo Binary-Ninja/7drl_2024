0f15dfdec93477b3ac89df328a26756eb3621e83
wolfw <wolfwwetzel@gmail.com>
2024-03-01 15:47:41 -0800

    Added flashing direction indicator and UI elements.

--- data.py
+++ data.py
@@ -38,12 +38,70 @@ class TileTag(StrEnum):
 class Colors(Enum):
     GREEN = (0, 255, 0)
     YELLOW = (255, 255, 0)
+    DARK_YELLOW = (128, 128, 0)
     BLUE = (0, 0, 255)
     STONE = (170, 170, 170)
     WHITE = (255, 255, 255)
     RED = (255, 0, 0)
+    DARK_RED = (128, 0, 0)
     MOB_GREEN = (64, 200, 64)
 
+
+class Tile(Enum):
+    PLAYER = (25, 0)
+    ZOMBIE = (24, 9)
+    SLIME = (27, 8)
+    SKELETON = (29, 6)
+    AIR_WIZARD = (24, 1)
+
+    HEART_FULL = (42, 10)
+    HEART_EMPTY = (40, 10)
+    STAM_FULL = (42, 12)
+    STAM_EMPTY = (40, 12)
+
+    CURSOR = (36, 12)
+
+
+str_2_tile = {
+    "0": (35, 17),
+    "1": (36, 17),
+    "2": (37, 17),
+    "3": (38, 17),
+    "4": (39, 17),
+    "5": (40, 17),
+    "6": (41, 17),
+    "7": (42, 17),
+    "8": (43, 17),
+    "9": (44, 17),
+    "a": (35, 18),
+    "b": (36, 18),
+    "c": (37, 18),
+    "d": (38, 18),
+    "e": (39, 18),
+    "f": (40, 18),
+    "g": (41, 18),
+    "h": (42, 18),
+    "i": (43, 18),
+    "j": (44, 18),
+    "k": (45, 18),
+    "l": (46, 18),
+    "m": (47, 18),
+    "n": (35, 19),
+    "o": (36, 19),
+    "p": (37, 19),
+    "q": (38, 19),
+    "r": (39, 19),
+    "s": (40, 19),
+    "t": (41, 19),
+    "u": (42, 19),
+    "v": (43, 19),
+    "w": (44, 19),
+    "x": (45, 19),
+    "y": (46, 19),
+    "z": (47, 19),
+    " ": (0, 0),
+
+}
 
 tile_graphics = {
     TileID.GRASS: ((5, 0), Colors.GREEN.value),
@@ -55,11 +113,11 @@ tile_graphics = {
 }
 
 mob_graphics = {
-    MobID.PLAYER: ((25, 0), Colors.WHITE.value),
-    MobID.GREEN_ZOMBIE: ((24, 9), Colors.MOB_GREEN.value),
-    MobID.GREEN_SLIME: ((27, 8), Colors.MOB_GREEN.value),
-    MobID.GREEN_SKELETON: ((29, 6), Colors.MOB_GREEN.value),
-    MobID.AIR_WIZARD: ((24, 1), Colors.RED.value),
+    MobID.PLAYER: (Tile.PLAYER.value, Colors.WHITE.value),
+    MobID.GREEN_ZOMBIE: (Tile.ZOMBIE.value, Colors.MOB_GREEN.value),
+    MobID.GREEN_SLIME: (Tile.SLIME.value, Colors.MOB_GREEN.value),
+    MobID.GREEN_SKELETON: (Tile.SKELETON.value, Colors.MOB_GREEN.value),
+    MobID.AIR_WIZARD: (Tile.AIR_WIZARD.value, Colors.RED.value),
 }
 
 tile_tags = defaultdict(tuple)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pygame as pg
 
 from tileloader import TileLoader
 from world import World, set_array, get_array
-from data import (MobID, Point, MobData, tile_graphics,
+from data import (MobID, Point, MobData, tile_graphics, str_2_tile, Colors, Tile,
                   PointType, TileTag, tile_tags, mob_graphics)
 
 
@@ -21,7 +21,15 @@ def main():
 
     tile_size = Point(16, 16)
     tile_loader = TileLoader(Path() / "kenney_tileset.png", tile_size)
-    player_tile = tile_loader.get_tile((25, 0))
+    heart_full_img = tile_loader.get_tile(Tile.HEART_FULL.value, Colors.RED.value)
+    heart_empty_img = tile_loader.get_tile(Tile.HEART_EMPTY.value, Colors.DARK_RED.value)
+    stam_full_img = tile_loader.get_tile(Tile.STAM_FULL.value, Colors.YELLOW.value)
+    stam_empty_img = tile_loader.get_tile(Tile.STAM_EMPTY.value, Colors.DARK_YELLOW.value)
+    cursor_img = tile_loader.get_tile(Tile.CURSOR.value, Colors.WHITE.value)
+
+    cursor_flash_timer = pg.time.get_ticks()
+    CURSOR_FLASH_FREQ = 500
+    cursor_show = True
 
     world_seed = random.getrandbits(64)
     world_seed = 1234
@@ -30,6 +38,9 @@ def main():
     game_world.generate_overworld_layer()
 
     player_vision = 17
+    player_health = 10
+    player_stamina = 10
+    player_dir = Point(0, -1)
     player_pos = Point(game_world.size[0] // 2, game_world.size[1] // 2)
     set_array(player_pos, game_world.overworld_layer.mob_array, MobData(MobID.PLAYER, 10))
 
@@ -43,6 +54,12 @@ def main():
         spawn_mob((_ * 2, 4), MobID.GREEN_SKELETON)
         spawn_mob((_ * 2, 6), MobID.AIR_WIZARD)
 
+    def write_text(pos: PointType, text: str, color):
+        for index, char in enumerate(text):
+            char_tile = tile_loader.get_tile(str_2_tile[char], color)
+            screen.blit(char_tile, ((pos[0] + index) * tile_size[0],
+                                    pos[1] * tile_size.y))
+
     def get_array_tile(pos: PointType, array: list[list]) -> pg.Surface | None:
         value = get_array(pos, array)
         if value:
@@ -52,6 +69,9 @@ def main():
 
     def move_player(direction: tuple[int, int]) -> Point:
         try_pos = Point(player_pos.x + direction[0], player_pos.y + direction[1])
+        move_mob = get_array(try_pos, game_world.overworld_layer.mob_array)
+        if move_mob:
+            return player_pos
         move_tile = get_array(try_pos, game_world.overworld_layer.tile_array)
         if move_tile is None or TileTag.BLOCK_MOVE in tile_tags[move_tile]:
             return player_pos
@@ -70,16 +90,36 @@ def main():
                     pg.quit()
                     sys.exit()
                 elif event.key == pg.K_UP:
-                    player_pos = move_player((0, -1))
+                    cursor_show = True
+                    if player_dir == (0, -1):
+                        player_pos = move_player((0, -1))
+                    else:
+                        player_dir = Point(0, -1)
                 elif event.key == pg.K_DOWN:
-                    player_pos = move_player((0, 1))
+                    cursor_show = True
+                    if player_dir == (0, 1):
+                        player_pos = move_player((0, 1))
+                    else:
+                        player_dir = Point(0, 1)
                 elif event.key == pg.K_LEFT:
-                    player_pos = move_player((-1, 0))
+                    cursor_show = True
+                    if player_dir == (-1, 0):
+                        player_pos = move_player((-1, 0))
+                    else:
+                        player_dir = Point(-1, 0)
                 elif event.key == pg.K_RIGHT:
-                    player_pos = move_player((1, 0))
+                    cursor_show = True
+                    if player_dir == (1, 0):
+                        player_pos = move_player((1, 0))
+                    else:
+                        player_dir = Point(1, 0)
 
         # Update.
         clock.tick()
+
+        if pg.time.get_ticks() - cursor_flash_timer >= CURSOR_FLASH_FREQ:
+            cursor_flash_timer = pg.time.get_ticks()
+            cursor_show = not cursor_show
 
         # Draw.
         screen.fill((0, 0, 0))
@@ -101,8 +141,20 @@ def main():
                 dy += 1
             dx += 1
 
-        # Display player (temp until display mobs is made).
-        screen.blit(player_tile, (tile_size.x * 17, tile_size.y * 17))
+        # Draw facing cursor.
+        if cursor_show:
+            screen.blit(cursor_img, ((17 + player_dir.x) * tile_size.x,
+                                     (17 + player_dir.y) * tile_size.y))
+
+        # Draw UI.
+        write_text((35, 0), "life", Colors.WHITE.value)
+        for i in range(10):
+            tile = heart_empty_img if i >= player_health else heart_full_img
+            screen.blit(tile, ((40 + i) * tile_size[0], 0))
+        write_text((35, 1), "stam", Colors.WHITE.value)
+        for i in range(10):
+            tile = stam_empty_img if i >= player_stamina else stam_full_img
+            screen.blit(tile, ((40 + i) * tile_size[0], tile_size[1]))
 
         # Display FPS.
         fps_surf = font.render(str(clock.get_fps()), True, (255, 255, 255))
